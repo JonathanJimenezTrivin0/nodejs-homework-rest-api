27e61ac9049dae6a13e2109d98c3fb015b5a3b98
Jonathan Alexis Jiménez Triviño <jonnyj1216@gmail.com>
2024-01-11 15:13:08 -0500

    hw03-mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require("./app");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+// mongodb+srv://jonathan:f-ij.z6B2CDEa5m@cluster0.fqnphkb.mongodb.net/
